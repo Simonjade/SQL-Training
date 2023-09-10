f748b15d0d2deaa7403b2c5dc4834d1fcf2d1da2
Simonjade <jade.simon13@gmail.com>
2023-09-10 15:15:18 +0200

    feat: add function

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # SQL-Training
+
+## Explications
+
+Ce training SQL a été réalisé lors de ma formation à O'Clock. Il a nécéssité d'importer une base de données via un backup (fichier invisible pour ce repo public). Dans le dossier script, vous trouverez des exemples de requêtes SQL/views/function/regex réalisées pour m'entrainer et perfectionner ma maîtrise du SQL
+
+## Structure de la BDD
+
+Il y a trois tables :
+
+- person : nom et prénom de personnes ainsi que l'id de la ville dans laquelle ils habitent
+- city : nom des villes
+- relationship : il y a une colonne qui contient l'id du parent et une colonne qui contient de l'enfant.
---
+++ script/function.sql
@@ -0,0 +1,63 @@
+-- fonction pour retourner la population par ville
+CREATE OR REPLACE FUNCTION cities_population() RETURNS TABLE("Nom de la ville" text,Population int) AS $$
+SELECT city.name,COUNT(city.id)::int FROM city
+JOIN person ON person.city_id = city.id
+GROUP BY city.name;
+$$ LANGUAGE SQL
+
+-- fonction pour retourner la population d'une ville précise
+CREATE OR REPLACE FUNCTION city_population(s text) RETURNS int AS $$
+SELECT COUNT(person.id)::int FROM person
+JOIN city ON person.city_id = city.id
+WHERE city.name = s
+$$ LANGUAGE SQL
+
+-- fonction qui retourne les deux parents d'un enfant
+CREATE OR REPLACE FUNCTION get_parent(c int) RETURNS TABLE(prenom text, nom text) AS $$
+SELECT firstname,lastname FROM person
+JOIN relationship ON relationship.parent = person.id
+WHERE child = c
+$$ LANGUAGE SQL
+
+-- fonction qui retourne un parent ou un enfant suivant son statut
+CREATE OR REPLACE FUNCTION get_status(f text,l text) RETURNS text AS $$
+DECLARE r RECORD;
+BEGIN
+SELECT child,parent,person.id INTO r FROM person
+JOIN relationship ON child = person.id OR parent = person.id
+WHERE firstname = f AND lastname = l
+LIMIT 1;
+
+IF r.child = r.id
+THEN RETURN 'enfant';
+ELSE RETURN 'parent';
+END IF;
+
+END;
+$$ LANGUAGE plpgsql
+
+-- fonction qui retourne l'ensemble de la famille d'une personne suivant son nom/prenom
+CREATE OR REPLACE FUNCTION get_relatives(f text,l text) RETURNS TABLE("prénom" text,"nom" text) AS $$
+DECLARE t text;
+DECLARE id_temp int;
+BEGIN
+SELECT get_status(f,l) into t;
+IF t = 'enfant'
+THEN
+    RETURN QUERY SELECT firstname,lastname FROM person
+    WHERE id IN (
+        SELECT relationship.parent FROM person
+        JOIN relationship ON child = person.id
+        WHERE firstname = f AND lastname = l
+    );
+ELSE
+    RETURN QUERY SELECT firstname,lastname FROM person
+    WHERE id IN (
+        SELECT relationship.child FROM person
+        JOIN relationship ON parent = person.id
+        WHERE firstname = f AND lastname = l
+    );
+END IF;
+
+END;
+$$ LANGUAGE plpgsql
